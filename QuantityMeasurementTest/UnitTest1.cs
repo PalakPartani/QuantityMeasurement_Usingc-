f9032e6e5d2e92100cefe3f744645f04280129b3
PalakPartani <partanipalak16@gmail.com>
2020-08-13 07:03:39 -0700

    [Palak Partani]ADDED:Feature to check equality on null

--- QuantityMeasurementTest/UnitTest1.cs
+++ QuantityMeasurementTest/UnitTest1.cs
@@ -13,9 +13,14 @@ namespace QuantityMeasurementTest
         [Test]
         public void GivenFeetValues_WhenSame_ShouldReturnEqual()
         {
-           double value1 = quantityMeasurement.GetValue(0.0);
-           double value2= quantityMeasurement.GetValue(0.0);
+            double value1 = quantityMeasurement.GetValue(0.0);
+            double value2 = quantityMeasurement.GetValue(0.0);
             Assert.AreEqual(value1, value2);
+        }
+        [Test]
+        public void givenNullFeetValue_ShouldReturnFalse()
+        {
+            Assert.IsFalse(quantityMeasurement.Equals(null));
         }
     }
 }
