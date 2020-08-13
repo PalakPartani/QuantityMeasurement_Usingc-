0c0588f8846a7d1ecc4d69b4ac1e663b7959f84f
PalakPartani <partanipalak16@gmail.com>
2020-08-13 07:15:29 -0700

    [Palak Partani]ADDED:Feature to check equality on reference

--- QuantityMeasurementTest/UnitTest1.cs
+++ QuantityMeasurementTest/UnitTest1.cs
@@ -1,13 +1,20 @@
 using NUnit.Framework;
+using System;
+
 namespace QuantityMeasurementTest
 {
     public class Tests
     {
         QuantityMeasurement.QuantityMeasurement quantityMeasurement;
+        QuantityMeasurement.QuantityMeasurement quantityMeasurement2;
+
+
         [SetUp]
         public void Setup()
         {
             quantityMeasurement = new QuantityMeasurement.QuantityMeasurement();
+            quantityMeasurement2 = new QuantityMeasurement.QuantityMeasurement();
+
         }
 
         [Test]
@@ -17,10 +24,21 @@ namespace QuantityMeasurementTest
             double value2 = quantityMeasurement.GetValue(0.0);
             Assert.AreEqual(value1, value2);
         }
+
         [Test]
-        public void givenNullFeetValue_ShouldReturnFalse()
+        public void GivenNullFeetValue_ShouldReturnFalse()
         {
             Assert.IsFalse(quantityMeasurement.Equals(null));
         }
+
+        [Test]
+        public void givenReferenceObject_WhenEqual_ShouldReturnTrue()
+        {
+            double value1 = quantityMeasurement.GetValue(0.0);
+            double result=quantityMeasurement2.GetValue(0.0);
+            Boolean data= (value1.Equals(result));
+            Assert.IsTrue(data);
+        }
+
     }
 }
